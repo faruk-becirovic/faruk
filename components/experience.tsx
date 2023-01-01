9ae7c3b3ae999e184ff8907ad279a539a5d70148
F Becirovic <farukbec@icloud.com>
2023-01-01 11:04:15 +0100

    Added resume

---
+++ components/experience.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Experience(props) {
+    return (
+        <div className={props.className}>
+            <h1>{props.position}</h1>
+            <h2>{props.institute}</h2>
+            <h3>{props.time}</h3>
+            <Link href={props.href}>{props.link}</Link>
+            <p>{props.children}</p>
+        </div>
+    );
+}
